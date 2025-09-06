50767f0225e0ed28a249ba743a55f477a9562884
IT 75/2020 Kopuz Dražen <drazen.kopuz9@gmail.com>
2025-09-06 16:03:25 +0200

    Docker tested

--- CurrencyExchange/src/main/java/currencyExchange/implementation/CurrencyExchangeServiceImplementation.java
+++ CurrencyExchange/src/main/java/currencyExchange/implementation/CurrencyExchangeServiceImplementation.java
@@ -1,7 +1,5 @@
 package currencyExchange.implementation;
 
-import java.math.BigDecimal;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
